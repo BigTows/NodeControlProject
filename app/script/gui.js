5068092435ba53fb44cf9f2fb003141394c335f7
Alexander <gasfull98@gmail.com>
2017-02-11 15:59:55 +0300

    UPDATE: 1.0.0 BETA v2
    Add LiveReload

--- app/script/gui.js
+++ app/script/gui.js
@@ -2,12 +2,55 @@ var gui = require("nw.gui");
 var terminalWin = gui.Window;
 const spawn = require('child_process').spawn;
 const exec = require('child_process').exec;
+var fs = require('fs');
+/**
+ * recursive function to calculate size of directory
+ * @param   {string} path Text path
+ * @returns {number} folder size
+ */
+function getSumDir(path) {
+    var DIR = fs.readdirSync(path);
+    var Sum = 0;
+    for (var File in DIR) {
+        var NewPath = path + DIR[File];
+        try {
+            var stats = fs.statSync(NewPath);
+            if (stats.isDirectory()) Sum += getSumDir(NewPath + "/")
+            else Sum += stats.size;
+        } catch (err) {
+
+        }
+    }
+    return Sum;
+}
+/**
+ * Start Node process
+ * @param   {string} path Path to main file
+ * @param   {string} main Name main file
+ * @param   {object} body document on DOM
+ * @returns {object} spawn object
+ */
+function startNode(path, main, body) {
+    var node = spawn("cd " + path + " && /usr/local/bin/node " + main, {
+        shell: true
+    });
+    node.stdout.on('data', (data) => {
+        body.innerHTML += data + "<br>";
+    });
+    node.stderr.on('data', (data) => {
+        body.innerHTML += data + "<br>";
+    });
+    node.on('close', (code) => {
+        body.innerHTML += "Process finished with exit code: " + code + "<br>";
+    });
+    return node;
+}
 
 /**
  * Crate GUI with process node
- * @param {String} name Name project
- * @param {String} path Path to project folder
- * @param {String} main Name file 'starter'
+ * @param {string} name Name project
+ * @param {string} path Path to project folder
+ * @param {string} main Name file 'starter'
  */
 function Start(name, path, main) {
     terminalWin.open('views/terminal.html', {
@@ -15,21 +58,21 @@ function Start(name, path, main) {
         focus: true
     }, function (cWindows) {
         var node;
+        var IntervalID;
         cWindows.on('loaded', function () {
             var body = cWindows.window.document.body;
+            node = startNode(path, main, body);
             var Command = "node";
-            node = spawn("cd " + path + " && /usr/local/bin/node " + main, {
-                shell: true
-            });
-            node.stdout.on('data', (data) => {
-                body.innerHTML += data + "<br>";
-            });
-            node.stderr.on('data', (data) => {
-                body.innerHTML += data + "<br>";
-            });
-            node.on('close', (code) => {
-                body.innerHTML += "Process finished with exit code: " + code + "<br>";
-            });
+            var dirSum = getSumDir(path);
+            IntervalID = setInterval(function () {
+                if (dirSum != getSumDir(path)) {
+                    dirSum = getSumDir(path);
+                    body.innerHTML += "Reload...<br>";
+                    exec('kill ' + (node.pid + 1));
+                    node = startNode(path, main, body);
+                    body.innerHTML += "cd " + path + " && /usr/local/bin/node " + main;
+                }
+            }, 1500);
         });
         cWindows.on('close', function () {
             exec('kill ' + (node.pid + 1));
